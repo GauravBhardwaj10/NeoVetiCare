fead4fd8ec1690647977b12dca3658812796448f
Gaurav Bhardwaj <gauravs10sharma@gmail.com>
2020-09-19 11:07:16 -0400

    first commit

---
+++ app/src/main/java/com/demo/neoveticare/MainActivity.java
@@ -0,0 +1,14 @@
+package com.demo.neoveticare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstancegit initState);
+        setContentView(R.layout.activity_main);
+    }
+}
