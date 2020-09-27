f0662eedb41097a39855f2273d626bf3a43e937a
harpreetkaur-design <kaur06109@gmail.com>
2020-09-26 23:56:15 -0700

    Harpreet kaur 1992621

---
+++ app/src/main/java/com/demo/neoveticare/choosetimebyparentafedone.java
@@ -0,0 +1,14 @@
+package com.demo.neoveticare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class choosetimebyparentafedone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choosetimebyparentafedone);
+    }
+}
